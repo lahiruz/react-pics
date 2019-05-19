0351405fdf49dc0a31ee960d10af5bb8959c7aa0
Lahiru Jayamanna <lsampath999@gmail.com>
2019-05-19 21:16:17 +0530

    Convert search bar to a controlled component via state

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+	state = {searchTerm: ''};
+
 	render() {
 		return (
 			<div className='ui segment'>
 				<form className='ui form'>
 					<div className='field'>
 						<label>Image Search</label>
-						<input type='text' placeholder='Search image'/>
+						<input type='text'
+						       placeholder='Search image'
+						       value={this.state.searchTerm}
+						       onChange={e => this.setState({searchTerm: e.target.value})}/>
 					</div>
 				</form>
 			</div>
