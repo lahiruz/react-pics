df3de3f152033319b9de38d30352e8197e5c69e0
Lahiru Jayamanna <lsampath999@gmail.com>
2019-05-20 17:28:56 +0530

    Add axios dependency support to get image data

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,27 @@
 import React from 'react';
+import axios from 'axios';
 
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-	onSearchTermSubmit(term) {
-		console.log(term);
-	}
+	state = {results: []};
+
+	onSearchTermSubmit = async (term) => {
+		const imageResults = await axios.get('https://api.unsplash.com/search/photos', {
+			params: {query: term},
+			headers: {
+				Authorization: 'Client-ID cabf593cdb47d08a676abea7ed91b819d04e416357ad23f1059d5551257beab8'
+			}
+		});
+
+		this.setState({results: imageResults.data.results});
+	};
 
 	render() {
 		return (
 			<div className='ui container' style={{marginTop: '10px'}}>
 				<SearchBar onSearchTermSubmit={this.onSearchTermSubmit}/>
+				<h1>{this.state.results.length} images found</h1>
 			</div>
 		);
 	}
