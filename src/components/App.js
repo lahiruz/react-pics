76112a668d39e7164b4b1ad4006e3384d7fc35a6
Lahiru Jayamanna <lsampath999@gmail.com>
2019-06-10 17:19:54 +0800

    Add prettier support & formatting updates

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,28 @@
 import React from 'react';
 
-import unsplash from '../api/unsplash'
+import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
 import ImageList from './ImageList';
 
 class App extends React.Component {
-	state = {results: []};
+  state = { results: [] };
 
-	onSearchTermSubmit = async (term) => {
-		const imageResults = await unsplash.get('/search/photos', {
-			params: {query: term}
-		});
+  onSearchTermSubmit = async term => {
+    const imageResults = await unsplash.get('/search/photos', {
+      params: { query: term }
+    });
 
-		this.setState({results: imageResults.data.results});
-	};
+    this.setState({ results: imageResults.data.results });
+  };
 
-	render() {
-		return (
-			<div className='ui container' style={{marginTop: '10px'}}>
-				<SearchBar onSearchTermSubmit={this.onSearchTermSubmit}/>
-				<ImageList images={this.state.results}/>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: '10px' }}>
+        <SearchBar onSearchTermSubmit={this.onSearchTermSubmit} />
+        <ImageList images={this.state.results} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,30 +1,31 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-	state = {searchTerm: ''};
+  state = { searchTerm: '' };
 
-	onFromSubmit = event => {
-		event.preventDefault();
+  onFromSubmit = event => {
+    event.preventDefault();
 
-		this.props.onSearchTermSubmit(this.state.searchTerm);
-	};
+    this.props.onSearchTermSubmit(this.state.searchTerm);
+  };
 
-	render() {
-		return (
-			<div className='ui segment'>
-				<form className='ui form' onSubmit={this.onFromSubmit}>
-					<div className='field'>
-						<label>Image Search</label>
-						<input
-							type='text'
-							placeholder='Search image'
-							value={this.state.searchTerm}
-							onChange={e => this.setState({searchTerm: e.target.value})}/>
-					</div>
-				</form>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="ui segment">
+        <form className="ui form" onSubmit={this.onFromSubmit}>
+          <div className="field">
+            <label>Image Search</label>
+            <input
+              type="text"
+              placeholder="Search image"
+              value={this.state.searchTerm}
+              onChange={e => this.setState({ searchTerm: e.target.value })}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default SearchBar;
